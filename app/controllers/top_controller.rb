450f9d407c64f5f9b1e90bb1902ea72ce85d2fc6
3oc <TAKAHIDE@H.local>
2015-01-07 23:42:35 +0900

    hoge

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,22 @@
 class TopController < ApplicationController
   def show
+    if signed_in?
+      twitter = Twitter::REST::Client.new do |config|
+        config.consumer_key = "xXLIao0TrTyx2ZeXg2eNBygPw"
+        config.consumer_secret = "HJHc76kF6CqASNH0xrTDLQyoCR0lSU68TPwLb8Ix34bGyOYSKz"
+        config.access_token = current_user.token
+        config.access_token_secret = current_user.secret
+      end
+
+      updated_at = Time.parse(current_user.updated_at.to_s)
+      now = Time.now
+      if now - updated_at > 10000 || current_user.profile_image_url.nil?
+        user = twitter.user(current_user.username)
+        User.update current_user.id, profile_image_url: user.profile_image_url.to_s, name: user.name, location: user.location, description: user.description, friends_count: user.friends_count, followers_count: user.followers_count
+      end
+    end
+
+
     username = params[:username]
     youtube_id = params[:youtube_id]
     if youtube_id
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,10 @@
 class UsersController < ApplicationController
-  def top
-    unless signed_in?
-      redirect_to user_omniauth_authorize_path(:twitter)
-      return
-    end
-
-    twitter = Twitter::REST::Client.new do |config|
-      config.consumer_key = "xXLIao0TrTyx2ZeXg2eNBygPw"
-      config.consumer_secret = "HJHc76kF6CqASNH0xrTDLQyoCR0lSU68TPwLb8Ix34bGyOYSKz"
-      config.access_token = current_user.token
-      config.access_token_secret = current_user.secret
-    end
-
-    @username = params[:username]
-
-    @post_id = params[:post_id]
-
-    @posts = Post.where(username: @username).order("created_at DESC")
-
-    unless @user
-      @user = twitter.user(@username)
-      return
-    end
-
-    @user = User.where(username: @username)[0]
-    updated_at = Time.parse(@user.updated_at.to_s)
-    now = Time.now
-    if now - updated_at > 10000
-      user = twitter.user(@username)
-      User.update @user.id, profile_image_url: user.profile_image_url.to_s, name: user.name, location: user.location, description: user.description, friends_count: user.friends_count, followers_count: user.followers_count
-      @user = User.find @user.id
-    end
-  end
-
   def secret
+    @username = current_user.username
+    if @username != "carp_joshi"
+      return
+    end
+
     twitter = Twitter::REST::Client.new do |config|
       config.consumer_key = "xXLIao0TrTyx2ZeXg2eNBygPw"
       config.consumer_secret = "HJHc76kF6CqASNH0xrTDLQyoCR0lSU68TPwLb8Ix34bGyOYSKz"
@@ -41,7 +12,6 @@ class UsersController < ApplicationController
       config.access_token_secret = current_user.secret
     end
 
-    @username = current_user.username
 
 =begin
     friend_ids = twitter.friend_ids @username 
@@ -86,14 +56,15 @@ class UsersController < ApplicationController
   end
 
   def secret2
+    if @username != "carp_joshi"
+      return
+    end
     twitter = Twitter::REST::Client.new do |config|
       config.consumer_key = "xXLIao0TrTyx2ZeXg2eNBygPw"
       config.consumer_secret = "HJHc76kF6CqASNH0xrTDLQyoCR0lSU68TPwLb8Ix34bGyOYSKz"
       config.access_token = current_user.token
       config.access_token_secret = current_user.secret
     end
-
-    @username = current_user.username
 
     followers = twitter.follower_ids(params[:username], {count: 200})
     my_followers = twitter.follower_ids @username 
@@ -111,14 +82,15 @@ class UsersController < ApplicationController
   end
 
   def secret3
+    if @username != "carp_joshi"
+      return
+    end
     twitter = Twitter::REST::Client.new do |config|
       config.consumer_key = "xXLIao0TrTyx2ZeXg2eNBygPw"
       config.consumer_secret = "HJHc76kF6CqASNH0xrTDLQyoCR0lSU68TPwLb8Ix34bGyOYSKz"
       config.access_token = current_user.token
       config.access_token_secret = current_user.secret
     end
-
-    @username = current_user.username
 
     friend_ids = twitter.friend_ids @username 
     ids = Array.new
